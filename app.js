1ff3209dd95a9b3d5bc442d448765594cfdf5e66
JoaquinPettinari <joaquinpettinari@hotmail.com>
2019-12-22 16:35:13 -0300

    Mejoras del boton

--- app.js
+++ app.js
@@ -1,49 +1,5 @@
-class Golondrina {
-    constructor() {
-      this._energia = 100
-    }
-  
-    energia() {
-      return this._energia
-    }
-  
-    volar(kilometros) {
-      this._energia -= kilometros * 2
-    }
-  
-    comer(gramos) {
-      this._energia += gramos
-    }
-  
-    estaDebil() {
-      return this._energia < 50
-    }
-  
-    estaFeliz() {
-      return this._energia > 90
-    }
-  
-    haceLoQueQuieras() {
-      if (this.estaFeliz()) {
-        this.volar(10)
-      }
-      else if (this.estaDebil()) {
-        comer(50)
-      }
-    }
+var contador = 0;
 
-    saludar(){
-        document.write("Holaaa")
-    }
-
-  
-    // agregar estaDebil, estaFeliz,
-    // haceLoQueQuieras
-    //
-    // esta debil si energia < 50
-    // esta feliz si energia > 90
-    // si está feliz vuela 10 kms, si está debil come 50 gramos
-  
-  }
-  
-  module.exports = { Golondrina }
+function AumentarContador(){
+    document.getElementById("valor").innerHTML = contador++;
+}
