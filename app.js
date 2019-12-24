f9eb280a927297c9837f15214cbdd8d087ce07cc
P4BL1SK1 <pablogerez2000@gmail.com>
2019-12-24 15:10:41 -0300

    loginCheck() works

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var contador = 2;
 function AumentarContador(){
     document.getElementById("valor").innerHTML = 15;
 }
-
+//TODO: esta shit no funciona idk why.
 function loginCheck() {
     alert("You are not registered.");
 }
